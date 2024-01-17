23203455d69b597ebe255a655f1910d65c4017e2
Johanes Raphael Nandaputra <johanesraphaeln@gmail.com>
2024-01-18 05:09:37 +0700

    Creating sketch for uploading to board

---
+++ sketch.ino
@@ -0,0 +1,56 @@
+#include <FirebaseArduino.h>
+#include <ESP8266WiFi.h>
+#include <Firebase.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+byte lampu = D5;
+int status;
+
+void setup() {
+  // put your setup code here, to run once:
+  // Create an instance of WiFiManager
+  WiFiManager wifiManager;
+
+  // Uncomment the line below if you want to reset the WiFi settings (useful during development)
+  // wifiManager.resetSettings();
+
+  // Start the WiFiManager configuration portal
+  if (!wifiManager.autoConnect("AutoConnectAP")) {
+    Serial.println("Failed to connect and hit timeout");
+    delay(3000);
+    // Reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  }
+
+  // Connected to Wi-Fi
+  Serial.println("Connected to Wi-Fi");
+
+  // Print local IP address
+  Serial.println(WiFi.localIP());
+
+  // Continue with the rest of your setup code
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.stream("/lamp");
+
+  pinMode(D5, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Firebase.failed()) {
+    Serial.print(Firebase.error());
+  }
+
+  status = Firebase.getInt("lamp");
+  Serial.print(status);
+  if (status > 1) {
+    digitalWrite(lampu, HIGH);
+  } else {
+    digitalWrite(lampu, LOW);
+  }
+
+  delay(50);
+}
